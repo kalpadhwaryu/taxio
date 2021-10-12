d3a761155a1c63668688ce83f768edcb8c16ff49
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2021-10-12 15:03:53 +0530

    Add Login Page

--- app/src/main/java/com/example/taxio/MainActivity.java
+++ app/src/main/java/com/example/taxio/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.taxio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
