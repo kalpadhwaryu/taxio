a1607cba649f1ace0c2824f44d4e5f3d184ab956
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2021-10-13 18:01:00 +0530

    Stored Signup info in firebase

--- app/src/main/java/com/example/taxio/Signup.java
+++ app/src/main/java/com/example/taxio/Signup.java
@@ -7,9 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
-    private Button callLogin;
+
+    private TextInputLayout username_signup,password_signup,confirm_password_signup;
+    private Button callLogin,signup_btn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +28,29 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         callLogin=findViewById(R.id.callLogin);
+
+        username_signup=findViewById(R.id.username_signup);
+        password_signup=findViewById(R.id.password_signup);
+        confirm_password_signup=findViewById(R.id.confirm_password_signup);
+        signup_btn=findViewById(R.id.signup_btn);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode =FirebaseDatabase.getInstance();
+                reference=rootNode.getReference("users");
+
+                //Get all the values
+                String username = username_signup.getEditText().getText().toString();
+                String password = password_signup.getEditText().getText().toString();
+                String confirmPassword = confirm_password_signup.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass(username,password);
+                reference.child(username).setValue(helperClass);
+
+                Toast.makeText(Signup.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         callLogin.setOnClickListener(new View.OnClickListener() {
             @Override
