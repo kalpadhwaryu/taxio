9416e8db016a1322f8c5b8648c7719520bc1090b
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2021-10-14 19:38:20 +0530

    Added Email, Phoneno in Signup

--- app/src/main/java/com/example/taxio/Signup.java
+++ app/src/main/java/com/example/taxio/Signup.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -15,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Signup extends AppCompatActivity {
 
-    private TextInputLayout username_signup,password_signup,confirm_password_signup;
+    private TextInputLayout username_signup,email_signup,phoneno_signup,password_signup;
     private Button callLogin,signup_btn;
 
     FirebaseDatabase rootNode;
@@ -28,11 +29,12 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         callLogin=findViewById(R.id.callLogin);
-
         username_signup=findViewById(R.id.username_signup);
+        email_signup=findViewById(R.id.email_signup);
+        phoneno_signup=findViewById(R.id.phoneno_signup);
         password_signup=findViewById(R.id.password_signup);
-        confirm_password_signup=findViewById(R.id.confirm_password_signup);
         signup_btn=findViewById(R.id.signup_btn);
+
 
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,13 +42,18 @@ public class Signup extends AppCompatActivity {
                 rootNode =FirebaseDatabase.getInstance();
                 reference=rootNode.getReference("users");
 
+                if (!validateUsername() | !validateEmail()| !validatePhoneno() | !validatePassword()){
+                    return;
+                }
+
                 //Get all the values
                 String username = username_signup.getEditText().getText().toString();
+                String email = email_signup.getEditText().getText().toString();
+                String phoneno = phoneno_signup.getEditText().getText().toString();
                 String password = password_signup.getEditText().getText().toString();
-                String confirmPassword = confirm_password_signup.getEditText().getText().toString();
 
-                UserHelperClass helperClass = new UserHelperClass(username,password);
-                reference.child(username).setValue(helperClass);
+                UserHelperClass helperClass = new UserHelperClass(username,email,phoneno,password);
+                reference.child(phoneno).setValue(helperClass);
 
                 Toast.makeText(Signup.this, "Signup Successful", Toast.LENGTH_SHORT).show();
             }
@@ -60,4 +67,91 @@ public class Signup extends AppCompatActivity {
             }
         });
     }
+
+    private Boolean validateUsername(){
+        String val = username_signup.getEditText().getText().toString();
+        String noWhiteSpaces="\\A\\w{4,20}\\z";
+
+        if(val.isEmpty()){
+            username_signup.setError("Field cannot be empty");
+            return false;
+        }
+        else if (val.length()>=15){
+            username_signup.setError("Username too long");
+            return false;
+        }
+        else if (!val.matches(noWhiteSpaces)){
+            username_signup.setError("White spaces are not allowed");
+            return false;
+        }
+
+        else{
+            username_signup.setError(null);
+            username_signup.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail(){
+        String val = email_signup.getEditText().getText().toString();
+        String emailPattern ="[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if(val.isEmpty()){
+            email_signup.setError("Field cannot be empty");
+            return false;
+        }
+        else if(!val.matches(emailPattern)){
+            email_signup.setError("Invalid Email Address");
+            return false;
+        }
+        else {
+            email_signup.setError(null);
+            email_signup.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePhoneno(){
+        String val = phoneno_signup.getEditText().getText().toString();
+        if(val.isEmpty()){
+            phoneno_signup.setError("Field cannot be empty");
+            return false;
+        }
+        else if(!(val.length()==10)){
+            phoneno_signup.setError("Enter 10 digits");
+            return false;
+        }
+        else {
+            phoneno_signup.setError(null);
+            phoneno_signup.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword(){
+        String val = password_signup.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if(val.isEmpty()){
+            password_signup.setError("Field cannot be empty");
+            return false;
+        }
+        else if(!val.matches(passwordVal)){
+            password_signup.setError("Password is too weak");
+            return false;
+        }
+        else{
+            password_signup.setError(null);
+            password_signup.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/taxio/UserHelperClass.java
+++ app/src/main/java/com/example/taxio/UserHelperClass.java
@@ -1,16 +1,17 @@
 package com.example.taxio;
 
 public class UserHelperClass {
-    String username,password;
+    String username,email,phoneno,password;
 
     public  UserHelperClass(){
 
     }
 
-    public UserHelperClass(String username, String password) {
+    public UserHelperClass(String username, String email, String phoneno, String password) {
         this.username = username;
+        this.email = email;
+        this.phoneno = phoneno;
         this.password = password;
-
     }
 
     public String getUsername() {
@@ -21,6 +22,22 @@ public class UserHelperClass {
         this.username = username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -28,6 +45,4 @@ public class UserHelperClass {
     public void setPassword(String password) {
         this.password = password;
     }
-
-
 }
