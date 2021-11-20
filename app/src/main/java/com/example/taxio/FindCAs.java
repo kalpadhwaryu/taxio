6e51d2d846c14b5a8fef87f3ddcb980ad802c6e5
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2021-11-20 17:40:24 +0530

    Find CAs in your city completed

--- app/src/main/java/com/example/taxio/FindCAs.java
+++ app/src/main/java/com/example/taxio/FindCAs.java
@@ -1,16 +1,17 @@
 package com.example.taxio;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,12 +21,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class FindCAs extends AppCompatActivity {
     private Spinner spinner;
     private Button search;
-    private TextView tv3;
+    private ListView lv_name,lv_phone,lv_email;
+//    private TextView tv3;
+
     private DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +39,17 @@ public class FindCAs extends AppCompatActivity {
 
         spinner=findViewById(R.id.spinner);
         search=findViewById(R.id.button);
-        tv3=findViewById(R.id.tv3);
+        lv_name=findViewById(R.id.lv_name);
+        lv_phone=findViewById(R.id.lv_phone);
+        lv_email=findViewById(R.id.lv_email);
+//        tv3=findViewById(R.id.tv3);
 
-        String[] items = new String[]{"Vadodara", "Ahmedabad","Surat", "Delhi", "Mumbai", "Chennai"};
+        String[] items = new String[]{"Vadodara", "Ahmedabad","Surat", "Delhi", "Mumbai"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         spinner.setAdapter(adapter);
 
         final String[] city = new String[1];
-
+        List<CAsHelperClass> list =new ArrayList<>();
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
@@ -62,7 +70,79 @@ public class FindCAs extends AppCompatActivity {
                 reference.child(city[0].toLowerCase(Locale.ROOT)).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        tv3.setText(snapshot.getValue().toString());
+                        list.clear();
+                        for (DataSnapshot ds:snapshot.getChildren()){
+//                            String name =ds.child("name").getValue(String.class);
+//                            String phone = ds.child("phone").getValue(String.class);
+//                            String email =ds.child("email").getValue(String.class);
+//                            list.add(name +"\n"+phone +"\n"+email+"\n");
+//                            tv3.setText(list.toString());
+                              CAsHelperClass ca = ds.getValue(CAsHelperClass.class);
+                              list.add(ca);
+                        }
+
+                        String[] CAname = new String[list.size()];
+                        for (int i=0;i<CAname.length;i++){
+                            CAname[i]=list.get(i).getName();
+                        }
+
+                        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,CAname){
+                            @NonNull
+                            @Override
+                            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                                View view = super.getView(position, convertView, parent);
+                                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                                textView.setTextColor(Color.BLACK);
+                                textView.setTextSize(12);
+                                return view;
+                            }
+                        };
+                        lv_name.setAdapter(arrayAdapter);
+
+
+
+
+                        String[] CAphone = new String[list.size()];
+                        for (int i=0;i<CAphone.length;i++){
+                            CAphone[i]=list.get(i).getPhone();
+                        }
+
+                        ArrayAdapter<String> arrayAdapter2=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,CAphone){
+                            @NonNull
+                            @Override
+                            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                                View view = super.getView(position, convertView, parent);
+                                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                                textView.setTextColor(Color.BLACK);
+                                textView.setTextSize(12);
+                                return view;
+                            }
+                        };
+                        lv_phone.setAdapter(arrayAdapter2);
+
+
+
+                        String[] CAemail = new String[list.size()];
+                        for (int i=0;i<CAemail.length;i++){
+                            CAemail[i]=list.get(i).getEmail();
+                        }
+
+                        ArrayAdapter<String> arrayAdapter3=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,CAemail){
+                            @NonNull
+                            @Override
+                            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                                View view = super.getView(position, convertView, parent);
+                                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                                textView.setTextColor(Color.BLACK);
+                                textView.setTextSize(12);
+                                return view;
+                            }
+                        };
+                        lv_email.setAdapter(arrayAdapter3);
+
                     }
 
                     @Override
@@ -70,10 +150,9 @@ public class FindCAs extends AppCompatActivity {
 
                     }
                 });
-//                if (city[0]=="Vadodara"){
-//
-//                }
+
             }
+
         });
 
     }
